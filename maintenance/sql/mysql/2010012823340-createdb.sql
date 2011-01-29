7df942d8983b47db5e155c9ae07c6fefcdc656f5
Nicholas Avenell <nicholas@aquarionics.com>
2011-01-29 01:10:54 +0000

    maintenance/sql/mysql/2010012823340-createdb.sql

--- maintenance/sql/mysql/2010012823340-createdb.sql
+++ maintenance/sql/mysql/2010012823340-createdb.sql
@@ -1,4 +1,4 @@
 drop database firecat;
 create database firecat;
-grant all on firecat.* to firecat@localhost identified by "catat0nic";
+grant all on firecat.* to firecat@localhost identified by "password";
 use firecat;
