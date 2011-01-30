e0bade2c903cfb88704524e0a04981ca95767b1b
Nicholas Avenell <nicholas@aquarionics.com>
2011-01-30 12:00:08 +0000

    New front page

--- libraries/Controller/FrontPage.php
+++ libraries/Controller/FrontPage.php
@@ -2,7 +2,11 @@
 
 class Controller_FrontPage extends Controller {
     function IndexAction(){
-        $this->response->setcontent("Hello World");
+        $smarty = new SmartyView();
+        $out = $smarty->assign('bodyStyle', "frontpage");
+        $out = $smarty->assign('title', "Firecat Maskerade");
+        $out = $smarty->fetch('frontpage.tpl.html');
+        $this->response->setcontent($out);
     }
 }
 
