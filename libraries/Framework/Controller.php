090d1f9e7bf16d036a304700a72a5e5fe2f71db2
Nicholas Avenell <nicholas@aquarionics.com>
2011-01-30 09:49:16 +0000

    Some useful functions for making controllers require login/admin

--- libraries/Framework/Controller.php
+++ libraries/Framework/Controller.php
@@ -14,4 +14,29 @@ class Controller {
 		$this->response->setError('Controller '.get_class($this).' doesn\'t have an action called '.$method);
 		$this->response->setStatus(404);
 	}
+
+
+    function requireLogin(){
+        $session = Session::getInstance();
+        if(!$session->get("loggedin")){
+            $session->set("afterLogin", $this->request->uri);
+            $this->response->redirect("/User/login");
+            return false;
+        }
+        return true;
+    }
+
+    function requireAdmin(){
+        $session = Session::getInstance();
+        if($this->requireLogin()){
+            $user = $session->get("user");
+            if ($user->is_admin){
+                return true;
+            } else {
+                throw Exception("Access Denied");
+            }
+        } else {
+            return false;
+        }
+    }
 }
